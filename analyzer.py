8b62d101c67048883ba9769b958466a95ee7fb87
WeonMaster3000 <maximomdalidet@gmail.com>
2026-02-24 14:49:25 -0300

    Add files via upload

--- analyzer.py
+++ analyzer.py
@@ -4,23 +4,42 @@ from scoring import score
 
 def analyzer(URL,H):
 
-    founds = []
+    sign = 0
     scor = 0
+    count = 0
 
     shorteners = r"bit\.ly|tinyurl\.com|goo\.su|t\.co"
-    Pattern = f"https?://({shorteners})/[a-zA-Z0-9]+"
+    pattern = f"https?://({shorteners})/[a-zA-Z0-9]+"
+    patternIp4 = r'\b(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\b'
 
-    if re.findall(Pattern, URL):        
+
+    for search in URL:
+        if "-" in search:
+            count += 1
+
+    if count >= 4:
+            scor += 5
+            sign += 1
+            print("[!] too much -")   
+
+    if re.search(patternIp4, URL):
         scor += 10
-        founds.append("shorteners")
+        sign += 1
+        print("[!] detected ip4")
+
+    if re.findall(pattern, URL):        
+        scor += 15
+        sign += 1
+        print("[!] shortener in the url")
 
     if "http://" in URL:
-        scor += 5
-        founds.append("http")
+        scor += 10
+        sign += 1
+        print("[!] http in the url")
     
-    if H == True:
+    if H:
         response = requests.get(URL)
-        print(response.status_code)
-    
+        print(response.text)
+        print(response.status_code)   
 
-    score(founds,scor)
+    score(sign,scor)
--- scoring.py
+++ scoring.py
@@ -1,17 +1,11 @@
-def score(found,score):
-    signs = 0
-    for search in found:
-        if "shorteners" in search:
-            print("[*] Un enlace acortado no siempre es phishing; depende del contexto y la situación. Ante la duda, no lo abras y consulta con tu equipo.")
-            signs += 1        
-        if "http" in search:
-            print("[!] Si un enlace usa HTTP, no compartas datos sensibles; depende del contexto y la situación. Ante la duda, no lo abras y consulta con tu equipo.")
-            signs += 1
+def score(signs,score):
 
-    if score <= 30:
+    if score <= 20:
         print(f"[*] possible low risk {signs} possible signs of phising")
-    elif score >= 80:
-        print(f"[!] possible hight risk {signs} possible signs of phising")
+    elif score <= 50:
+        print(f"[:] possible mid-low risk {signs} possible signs of phising")
+    elif score <= 75:
+        print(f"[!] possible mid risk {signs} possible signs of phising")
     else:
-        print(f"[:] possible mid risk {signs} possible signs of phising")
+        print(f"[!!] possible hight risk {signs} possible signs of phising")
      
