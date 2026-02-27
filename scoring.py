766a52fcaa2bf077c2a1f5a98777b61dc2f0eea4
WeonMaster3000 <maximomdalidet@gmail.com>
2026-02-27 15:03:41 -0300

    Add files via upload

--- scoring.py
+++ scoring.py
@@ -1,11 +1,9 @@
 def score(signs,score):
 
-    if score <= 20:
+    if score <= 15:
         print(f"[*] possible low risk {signs} possible signs of phising")
-    elif score <= 50:
-        print(f"[:] possible mid-low risk {signs} possible signs of phising")
-    elif score <= 75:
-        print(f"[!] possible mid risk {signs} possible signs of phising")
-    else:
-        print(f"[!!] possible hight risk {signs} possible signs of phising")
-     
+    elif score <= 25:
+        print(f"[:] possible mid risk {signs} possible signs of phising")
+    elif score <= 45:
+        print(f"[!] possible hight risk {signs} possible signs of phising")
+    
